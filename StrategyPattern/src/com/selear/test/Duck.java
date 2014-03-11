be0d46dd96a7a072d7cbbcb3053271beb57730c1
selear <selear@gmail.com>
2014-03-11 21:28:23 +0800

    让鸭子飞起来
    希望所有鸭子能够飞起来：为鸭子超类添加fly()方法

--- StrategyPattern/src/com/selear/test/Duck.java
+++ StrategyPattern/src/com/selear/test/Duck.java
@@ -2,17 +2,20 @@ package com.selear.test;
 
 public class Duck {
 
-	//所有鸭子都会呱呱叫
+	// 所有鸭子都会呱呱叫
 	void quack() {
 	}
 
-	//所有的鸭子都会游泳
+	// 所有的鸭子都会游泳
 	void swim() {
 	}
 
-	//每一种鸭子的外观都不同, 因此display()方法是抽象的
+	// 每一种鸭子的外观都不同, 因此display()方法是抽象的
 	void display() {
 	}
 
-	//鸭子的其他方法
+	// 我们希望鸭子能飞,所有的子类都会继承fly()方法
+	void fly() {
+
+	}
 }
